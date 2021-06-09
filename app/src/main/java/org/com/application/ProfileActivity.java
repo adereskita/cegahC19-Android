a71526ade1e722f54f70ad961a9749505a6d367f
Ade Reskita <ade.reskita@yahoo.com>
2021-06-09 20:10:49 +0700

    Initial Commit

---
+++ app/src/main/java/org/com/application/ProfileActivity.java
@@ -0,0 +1,14 @@
+package org.com.application;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
