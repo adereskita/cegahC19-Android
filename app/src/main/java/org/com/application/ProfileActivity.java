12c109261bafb5f9003f44da8dd5d3f435e66370
Richardo4039 <christian.paskalis@gmail.com>
2021-06-15 20:44:05 +0700

    Tambah MapsActivity + acitivty_maps

--- app/src/main/java/org/com/application/ProfileActivity.java
+++ app/src/main/java/org/com/application/ProfileActivity.java
@@ -1,14 +1,31 @@
 package org.com.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+    Button nearby_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //define nearby_button
+        nearby_button = findViewById(R.id.btnFind_nearby);
+
+        //ketika di klik membuka maps nearby
+        nearby_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
