2094bbc50c656ae6b4f447cf7dedb31f6dcef0db
Ade Reskita <ade.reskita@yahoo.com>
2021-06-25 01:02:52 +0700

    Articel get

--- app/src/main/java/org/com/application/Adapter/RecyclerArticleAdapter.java
+++ app/src/main/java/org/com/application/Adapter/RecyclerArticleAdapter.java
@@ -2,14 +2,11 @@ package org.com.application.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,7 +15,6 @@ import com.squareup.picasso.Picasso;
 
 import org.com.application.ArticleActivity;
 import org.com.application.HomeActivity;
-import org.com.application.LoginActivity;
 import org.com.application.Model.PostModel;
 import org.com.application.R;
 
@@ -26,16 +22,15 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+public class RecyclerArticleAdapter extends RecyclerView.Adapter<RecyclerArticleAdapter.ViewHolder> {
 
     private ArrayList<PostModel> data;
     private Context mContext;
 
-    private final int limit = 5;
+    private String id;
 
-    public RecyclerViewAdapter(ArrayList<PostModel> data, Context mContext) {
+    public RecyclerArticleAdapter(ArrayList<PostModel> data, Context mContext) {
         this.data = data;
         this.mContext = mContext;
     }
@@ -67,11 +62,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.date.setText(new_Date);
         Picasso.get().load(HomeActivity.URL_BASE_STORAGE+nPost.getImage()).into(holder.imgView);
 
+        id = nPost.getId();
+
     }
 
     @Override
     public int getItemCount() {
-        return Math.min(data.size(), limit);
+        return data.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -83,6 +80,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             title = itemView.findViewById(R.id.tv_rv_title);
             date = itemView.findViewById(R.id.tv_rc_date);
             imgView = itemView.findViewById(R.id.img_home_post);
+
+            itemView.isClickable();
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int itemPosition = getAbsoluteAdapterPosition();
+
+                    PostModel nPostId = data.get(itemPosition);
+                    id = nPostId.getId();
+
+                    Intent i = new Intent(mContext.getApplicationContext(), ArticleActivity.class);
+                    i.putExtra("EXTRA_ID_POST",id);
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    mContext.getApplicationContext().startActivity(i);
+                }
+            });
         }
     }
 }
