9442b0f3017cfb526b51e2df2bac7c2cb6b6c1af
ALFajri <alfajri268@gmail.com>
2021-06-28 13:42:01 +0700

    Update Java and Xml Form Gejala

--- app/src/main/java/org/com/application/FormGejalaActivity.java
+++ app/src/main/java/org/com/application/FormGejalaActivity.java
@@ -1,8 +1,17 @@
 package org.com.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,6 +26,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.com.application.API.InterfaceAPI;
+import org.com.application.API.RetrofitClient;
+import org.com.application.Model.CovidModel;
+import org.com.application.Model.UserModel;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,34 +38,129 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FormGejalaActivity extends AppCompatActivity {
+
     private static final String ipaddressLaravel = "10.0.2.2:8000";
-    private static final String URL_GET_USER = "http://"+ipaddressLaravel+"/api/auth/user";
+    private static final String URL_GET_USER = "http://" + ipaddressLaravel + "/api/auth/user";
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor meditor;
+
+    private int id;
+
+    private CheckBox cbdemam, cbbatuk, cbdiare, cblemes, cbkakupundak,cbmatakuning,cbkulitruam, cbmatamerah,cbsesaknafas;
+    EditText et_name, et_age,et_gender, et_nik, et_telfo,et_provinsi, et_kota, et_address, et_gejala;
+    Spinner sp_gender;
+    Button btnsave;
 
     private static String ACCESS_TOKEN;
 
-
-
-    EditText et_name, et_age, et_nik, et_telfo,et_address;
-    Spinner sp_gender;
-    Button btn_save;
-
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_gejala);
 
 
-    //initial variable
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(FormGejalaActivity.this);
+        meditor = mPreferences.edit();
+
+        System.out.println("ID DI HOME: "+id);
+
+        //initial variable
         et_name = findViewById(R.id.edt_nama_reg);
         et_age = findViewById(R.id.edt_age);
+        sp_gender = findViewById(R.id.sp_gender);
         et_nik = findViewById(R.id.edt_nik);
         et_telfo = findViewById(R.id.edt_no_telfon);
+        et_provinsi = findViewById(R.id.edt_provinsi);
         et_address = findViewById(R.id.edt_alamat);
+//        et_gejala = findViewById(R.id.edt_g)
+
+        setCheckBoxListener();
+        //setButtonListener();
+
+        LoadUser();
+
 
     }
 
+    private void setCheckBoxListener() {
+        cbdemam = (CheckBox) findViewById(R.id.cb_demam);
+        cbbatuk= (CheckBox) findViewById(R.id.cb_batuk);
+        cbdiare = (CheckBox) findViewById(R.id.cb_diare);
+        cbkakupundak = (CheckBox) findViewById(R.id.cb_kuku_ruam);
+        cbkulitruam= (CheckBox) findViewById(R.id.cb_kulit_ruam);
+        cbmatamerah = (CheckBox) findViewById(R.id.cb_mata_merah);
+        cbmatakuning = (CheckBox) findViewById(R.id.cb_matakuning);
+        cblemes = (CheckBox) findViewById(R.id.cb_lemas);
+        cbsesaknafas = (CheckBox) findViewById(R.id.cb_sesak_nafas);
 
-    private void LoadUser(){
+        btnsave = (Button) findViewById(R.id.btn_save);
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String status = "Demam " + cbdemam.isChecked()
+                        + "\n Batuk " + cbbatuk.isChecked()
+                        + "\n Diare " + cbdiare.isChecked()
+                        + "\n Lemes " + cblemes.isChecked()
+                        + "\n Kaku Pundak " + cbkakupundak.isChecked()
+                        + "\n Mata Kuning " + cbmatakuning.isChecked()
+                        + "\n Kulit Ruam " + cbkulitruam.isChecked()
+                        + "\n Sesak Nafas " + cbsesaknafas.isChecked()
+                        + "\n Mata merah " + cbmatamerah.isChecked();
+
+                Toast.makeText(FormGejalaActivity.this,status, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private void postCovid() {
+        String nama = et_name.getText().toString().trim();
+        String umur = et_age.getText().toString().trim();
+        String nik = et_nik.getText().toString().trim();
+        String telepon = et_telfo.getText().toString().trim();
+
+
+//        Retrofit.Builder builder = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create());
+//        Retrofit retrofit = builder.build();
+
+        Retrofit retrofit = RetrofitClient.getRetrofitInstance();
+
+        InterfaceAPI apiClient = retrofit.create(InterfaceAPI.class);
+
+        CovidModel covid = new CovidModel(id, "nama", "umur", "gender", "nik", "telepon", "provinsi", "kota", "alamat", "gejala");
+        //Call<UserModel> call = apiClient.login("application/json","application/json",user);
+        Call<CovidModel> call = apiClient.covidPost(covid);
+
+        call.enqueue(new Callback<CovidModel>() {
+            @Override
+            public void onResponse(Call<CovidModel> call, retrofit2.Response<CovidModel> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        Toast.makeText(FormGejalaActivity.this, "Berhasil Input Data", Toast.LENGTH_SHORT).show();
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    Toast.makeText(FormGejalaActivity.this, "Input Data Gagal", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CovidModel> call, Throwable t) {
+                Toast.makeText(FormGejalaActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void LoadUser() {
         StringRequest stringRequest = new StringRequest(Request.Method.GET,
                 URL_GET_USER,
                 new Response.Listener<String>() {
@@ -62,12 +170,14 @@ public class FormGejalaActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(response);
                             JSONArray jsonArray = jsonObject.getJSONArray("data");
 
-                            for (int i = 0; i<jsonArray.length(); i++){
+                            for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject obj = jsonArray.getJSONObject(i);
 
                                 if (obj != null) {
                                     et_name.setText(obj.getString("name"));
+                                    System.out.println(obj.getString("name"));
                                     et_nik.setText(obj.getString("nik"));
+                                    System.out.println(obj.getString("nik"));
                                 }
                             }
                         } catch (JSONException e) {
@@ -78,7 +188,7 @@ public class FormGejalaActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }) {
 
